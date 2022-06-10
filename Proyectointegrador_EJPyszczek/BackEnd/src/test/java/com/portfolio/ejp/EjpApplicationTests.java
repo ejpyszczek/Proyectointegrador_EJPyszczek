96134d2ac0c4ffd3721b9538470ef352954b3e84
ejpyszczek <ejpyszczek@gmail.com>
2022-06-10 18:25:30 -0300

    Update - Login - Redes

---
+++ Proyectointegrador_EJPyszczek/BackEnd/src/test/java/com/portfolio/ejp/EjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ejp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
