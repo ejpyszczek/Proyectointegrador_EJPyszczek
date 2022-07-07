8c33c037723cfe1947f30856b6bb5e47dcd643fa
ejpyszczek <ejpyszczek@gmail.com>
2022-07-07 16:22:42 -0300

    Act backend

--- Proyectointegrador_EJPyszczek/BackEnd/src/main/java/com/portfolio/ejp/Security/jwt/JwtTokenFilter.java
+++ Proyectointegrador_EJPyszczek/BackEnd/src/main/java/com/portfolio/ejp/Security/jwt/JwtTokenFilter.java
@@ -34,7 +34,7 @@ public class JwtTokenFilter extends OncePerRequestFilter{
             if(token != null && jwtProvider.validateToken(token)){
                 String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
                 UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(nombreUsuario);
-                UsernamePasswordAuthenticationToken auth =new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(auth);
            }
         }catch (Exception e){
