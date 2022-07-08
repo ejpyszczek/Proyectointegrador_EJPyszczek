ee4fe5d0c361dfcf211940d3c26946b2d76d0003
ejpyszczek <ejpyszczek@gmail.com>
2022-07-08 00:52:26 -0300

    Act FrontEnd Login

--- Proyectointegrador_EJPyszczek/FrontEnd/src/app/model/persona.model.ts
+++ Proyectointegrador_EJPyszczek/FrontEnd/src/app/model/persona.model.ts
@@ -1,2 +1,13 @@
-export class Persona.Model {
+
+export class persona{
+    id?: number;
+    nombre: string;
+    apellido: string;
+    img: string;
+
+    constructor(nombre: string,apellido: string, img: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.img = img;
+    }
 }
